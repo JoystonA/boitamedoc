292806d233b10e2a6946cc81ae8530dd0c754b3f
ANTON RAVEENDRAN Joyston <joyston.antonraveendran@edu.esiee.fr>
2019-05-12 18:42:13 +0200

    "Ajout du bar de navigation fonctionnel" "Utilisation de Java et non de Kotlin car plus facile pour les boutons"

---
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.boitamedoc_v2;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_accueil:
+                    mTextMessage.setText(R.string.title_accueil);
+                    return true;
+                case R.id.navigation_traitement:
+                    mTextMessage.setText(R.string.title_traitement);
+                    return true;
+                case R.id.navigation_boite:
+                    mTextMessage.setText(R.string.title_boite);
+                    return true;
+                case R.id.navigation_libre_service:
+                    mTextMessage.setText(R.string.title_libre_service);
+                    return true;
+                case R.id.navigation_profil:
+                    mTextMessage.setText(R.string.title_profil);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
