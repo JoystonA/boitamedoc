6b9e73482336ffa5788d76edf4bf37b6fae12a10
33669 <theo.belin@edu.esiee.fr>
2019-05-13 15:24:46 +0200

    Nav bar ok

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/MainActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.boitamedoc_v2;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -10,43 +11,47 @@ import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_accueil:
-                    mTextMessage.setText(R.string.title_accueil);
-                    return true;
-                case R.id.navigation_traitement:
-                    mTextMessage.setText(R.string.title_traitement);
-                    return true;
-                case R.id.navigation_boite:
-                    mTextMessage.setText(R.string.title_boite);
-                    return true;
-                case R.id.navigation_libre_service:
-                    mTextMessage.setText(R.string.title_libre_service);
-                    return true;
-                case R.id.navigation_profil:
-                    mTextMessage.setText(R.string.title_profil);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setOnNavigationItemSelectedListener(navListener);
         Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_accueil:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_traitement:
+                    selectedFragment = new TraitementFragment();
+                    break;
+                case R.id.navigation_boite:
+                    selectedFragment = new boiteFragment();
+                    break;
+                case R.id.navigation_libre_service:
+                    selectedFragment = new LibreServiceFragment();
+                    break;
+                case R.id.navigation_profil:
+                    selectedFragment = new ProfilFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true;
+        }
+    };
+
+
 
 }
