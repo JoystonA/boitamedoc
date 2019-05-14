c54c5a8aa345c2e1df9cd21e85b56f4a916f928a
33669 <theo.belin@edu.esiee.fr>
2019-05-14 11:10:32 +0200

    Clique sur une case ouvre une nouvelle activitée pour les infos Medicament.

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InfoCaseActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InfoCaseActivity.java
@@ -1,23 +1,23 @@
 package com.example.boitamedoc_v2;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.view.MenuItem;
-import android.widget.TextView;
-//import android.widget.Toolbar;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+//import android.widget.Toolbar;
+
+public class InfoCaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new InfoCaseFragment()).commit();
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(navListener);
         Toolbar toolbar = findViewById(R.id.app_bar);
