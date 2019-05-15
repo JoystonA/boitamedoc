e3108512a6cc6cdcfb5db362ef471376fa0befac
33669 <theo.belin@edu.esiee.fr>
2019-05-15 15:42:04 +0200

    Boite avec texte déroullant ok sauf pop up + datamatrix Libre service OK sauf link avec boite physique

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InfoCaseFragment.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InfoCaseFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class InfoCaseFragment extends Fragment {
-
+private TextView scrolltxt;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_infocase, container,false);
+
+        View v = inflater.inflate(R.layout.fragment_infocase, container, false);
+
+        scrolltxt = (TextView) v.findViewById(R.id.textView3);
+        scrolltxt.setMovementMethod(new ScrollingMovementMethod());
+        return v;
     }
 }
