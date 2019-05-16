1b58b10f97bd34570567b3f2f19ea8a228ea201b
33669 <theo.belin@edu.esiee.fr>
2019-05-16 14:27:50 +0200

    Merge w/ JP OK avec bouton parametre ok sur toutes les activitys faite sur ma branche

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/ParametreActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/ParametreActivity.java
@@ -17,41 +17,5 @@ public class ParametreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_parametre);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_param, new InfoCaseFragment()).commit();
-        Button setting = findViewById(R.id.action_setting);
-        //setting.setOnClickListener(navListener);
-        //Toolbar toolbar = findViewById(R.id.app_bar);
-        //setSupportActionBar(toolbar);
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-
-            switch (item.getItemId()) {
-                case R.id.navigation_accueil:
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.navigation_traitement:
-                    selectedFragment = new TraitementFragment();
-                    break;
-                case R.id.navigation_boite:
-                    selectedFragment = new boiteFragment();
-                    break;
-                case R.id.navigation_libre_service:
-                    selectedFragment = new LibreServiceFragment();
-                    break;
-                case R.id.navigation_profil:
-                    selectedFragment = new ProfilFragment();
-                    break;
-            }
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-
-            return true;
-
-        }// Fin de onOptionItemSelected
-    };
 }
