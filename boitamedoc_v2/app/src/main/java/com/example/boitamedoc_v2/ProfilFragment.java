a664496962b5b125cb838edf237e1b28c2856c04
ANTON RAVEENDRAN Joyston <joyston.antonraveendran@edu.esiee.fr>
2019-05-22 17:40:45 +0200

    "Création de la Notification"

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/ProfilFragment.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/ProfilFragment.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class ProfilFragment extends Fragment {
+public class ProfilFragment extends Fragment implements View.OnClickListener {
     private Button modifButton;
 
     @Nullable
@@ -20,6 +20,23 @@ public class ProfilFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profil, container, false);
 
         modifButton = (Button) v.findViewById(R.id.modifier_compte);
+        modifButton.setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View V) {
+        switch (V.getId()) {
+            case R.id.modifier_compte:
+                openModifProfilActivity();
+                break;
+        }
+    }
+
+    public void openModifProfilActivity() {
+        Intent intent;
+        intent = new Intent(getActivity(), ModifProfilActivity.class);
+        startActivity(intent);
     }
 }
