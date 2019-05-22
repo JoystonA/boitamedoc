a9ef836de2fb6a0f544dfca3eb991a6c6b485c24
33669 <theo.belin@edu.esiee.fr>
2019-05-22 11:23:17 +0200

    JAR BDD installé

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
@@ -1,6 +1,23 @@
 package com.example.boitamedoc_v2;
 
 import android.app.Application;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class App extends Application {
+    public static final String URL_BDD = "https://phpmyadmin.cluster021.hosting.ovh.net/";
+    public static final String user = "boitamedmxadmin";
+    public static final String pawd = "E3esieeboitamx2019";
+    public static Connection  conn;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            conn = DriverManager.getConnection(URL_BDD, user, pawd);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
