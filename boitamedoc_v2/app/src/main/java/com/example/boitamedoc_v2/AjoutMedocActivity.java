e4b739d26b518261f596e5e47be0ae7c6b51f6c4
33669 <theo.belin@edu.esiee.fr>
2019-05-23 14:19:06 +0200

    Driver OK

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/AjoutMedocActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/AjoutMedocActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static com.example.boitamedoc_v2.App.conn;
+
 public class AjoutMedocActivity extends AppCompatActivity {
     private TextView testTxt;
 
@@ -13,10 +20,22 @@ public class AjoutMedocActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ajoutmedoc);
 
     testTxt = (TextView) findViewById(R.id.testTxt);
-    if(getIntent().getStringExtra("value")!= null) {
-        testTxt.setText("QRCODE = "+ getIntent().getStringExtra("value"));
+        try {
+            Statement state = conn.createStatement();
+
+            ResultSet result = state.executeQuery("SELECT COUNT(*) FROM boitamedmxadmin.medicament");
+
+            ResultSetMetaData resultMeta = result.getMetaData();
+            while(result.next()){
+                testTxt.setText("Test"+ result.getObject(0).toString() );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       // if(getIntent().getStringExtra("value")!= null) {
+       // testTxt.setText("QRCODE = "+ getIntent().getStringExtra("value"));
     }
-    else testTxt.setText("CA MARCHE PAS");
-    }
+    //else testTxt.setText("CA MARCHE PAS");
+    //}
 
 }
--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
@@ -1,11 +1,12 @@
 package com.example.boitamedoc_v2;
 
 import android.app.Application;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import android.util.Log;
+
+import java.sql.*;
 
 public class App extends Application {
-    public static final String URL_BDD = "https://phpmyadmin.cluster021.hosting.ovh.net/";
+    public static final String URL_BDD = "jdbc:mysql://boitamedmxadmin.mysql.db" ;
     public static final String user = "boitamedmxadmin";
     public static final String pawd = "E3esieeboitamx2019";
     public static Connection  conn;
@@ -14,10 +15,12 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Log.d("Théo", "onCreate: DRIVER OK");
             conn = DriverManager.getConnection(URL_BDD, user, pawd);
         }
         catch (Exception e){
-            e.printStackTrace();
+            Log.d("Théo test", e.getMessage());
         }
     }
 }
