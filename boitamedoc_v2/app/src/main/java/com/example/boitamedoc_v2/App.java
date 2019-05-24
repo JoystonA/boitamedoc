04b3fc164bc54f65c3dcfcc77b91cb2f8f891bb9
33669 <theo.belin@edu.esiee.fr>
2019-05-24 10:57:12 +0200

    Driver OK

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/App.java
@@ -6,8 +6,8 @@ import android.util.Log;
 import java.sql.*;
 
 public class App extends Application {
-    public static final String URL_BDD = "jdbc:mysql://boitamedmxadmin.mysql.db" ;
-    public static final String user = "boitamedmxadmin";
+    public static final String URL_BDD = "jdbc:mysql://185.31.40.43:3306/boitamedmxadmin_databases" ;
+    public static final String user = "184284";
     public static final String pawd = "E3esieeboitamx2019";
     public static Connection  conn;
 
@@ -15,12 +15,13 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.jdbc.Driver");
             Log.d("Théo", "onCreate: DRIVER OK");
             conn = DriverManager.getConnection(URL_BDD, user, pawd);
+            Log.d("Théo", "onCreate: CONNEXION OK");
         }
         catch (Exception e){
-            Log.d("Théo test", e.getMessage());
+            Log.d("Théo test", e.getMessage() + " "+ e.getClass() + " " + e.getCause());
         }
     }
 }
