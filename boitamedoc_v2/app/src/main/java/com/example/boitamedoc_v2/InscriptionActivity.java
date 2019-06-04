4cd9c9a55d81281cfb1b60e0e044c8150fc6e6d6
ANTON RAVEENDRAN Joyston <joyston.antonraveendran@edu.esiee.fr>
2019-06-04 14:20:29 +0200

    "Menu déroulant dans Ajout et Modif Traitement"

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InscriptionActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InscriptionActivity.java
@@ -2,12 +2,7 @@ package com.example.boitamedoc_v2;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
