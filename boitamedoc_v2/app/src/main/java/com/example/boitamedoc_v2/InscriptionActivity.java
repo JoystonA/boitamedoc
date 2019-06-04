605d0cf2d25c335b9d6013896f63cfb15eb34525
ANTON RAVEENDRAN Joyston <joyston.antonraveendran@edu.esiee.fr>
2019-06-04 15:44:03 +0200

    "Design v1 OK"

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InscriptionActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/InscriptionActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.santalu.maskedittext.MaskEditText;
 
 public class InscriptionActivity extends AppCompatActivity {
