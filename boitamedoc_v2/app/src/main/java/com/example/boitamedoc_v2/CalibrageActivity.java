6e924a1fbe6b4d8d3dd7704fcfc843a95cc187d5
ANTON RAVEENDRAN Joyston <joyston.antonraveendran@edu.esiee.fr>
2019-06-13 10:40:34 +0200

    "Bluetooth OK reception message"

--- boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/CalibrageActivity.java
+++ boitamedoc_v2/app/src/main/java/com/example/boitamedoc_v2/CalibrageActivity.java
@@ -1,27 +1,32 @@
 package com.example.boitamedoc_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 
 public class CalibrageActivity extends AppCompatActivity {
 
+    private TextView masseUnComprimé;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibrage);
         setTitle("Calibrage Capteur de Masse");
-        messageConnexion();
-
+        masseUnComprimé = (TextView) findViewById(R.id.masseUnComprimé);
+        messageMasse();
     }
 
-    public void messageConnexion() {
+    public void messageMasse() {
 
         App.bluetooth_main.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
             public void onDataReceived(byte[] data, String message) {
                 Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                masseUnComprimé.setText(message+" g");
             }
         });
     }
